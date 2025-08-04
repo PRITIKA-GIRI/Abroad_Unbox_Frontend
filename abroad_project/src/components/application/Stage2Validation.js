2f956db8cbea22d509d79ff3da17afe1fca73fc7
Neetu Rai <raineetu0070@gmail.com>
2025-08-04 17:26:57 +0545

    refactor: code refactor

--- abroad_project/src/components/application/Stage2Validation.js
+++ abroad_project/src/components/application/Stage2Validation.js
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const getTodayDate = () => {
   const today = new Date();
-  return today.toISOString().split("T")[0]; // 'yyyy-mm-dd'
+  return today.toISOString().split("T")[0];
 };
 
 export const stage2Schema = z
